68d757646c07791f8c61dc9e12d535d8bc9fb520
Xannix246 <62834423+Xannix246@users.noreply.github.com>
2024-04-15 20:12:23 +0300

    Update ListenerLoader.ts

--- modules/ListenerLoader.ts
+++ modules/ListenerLoader.ts
@@ -1,4 +1,4 @@
-import { Client, Events } from "discord.js";
+import { Client, Events, GuildMember, GuildMemberRoleManager } from "discord.js";
 import { ListenMessage } from "global";
 const data = require('../data/data.json');
 
@@ -6,13 +6,21 @@ const ListenerLoader = async (client: Client) => {
     const listeners = require(`../data/servers/${data.GuildId}.json`);
 
     client.on(Events.MessageReactionAdd, async (reaction, user) => {
-        if(user.bot) return;
+        if (user.bot) return;
         if (reaction.message.guildId == null) return console.log('not found');
-    
+
         const guilds = client.guilds.cache.get(reaction.message.guildId);
         const member = guilds?.members.cache.get(user.id);
-    
+
         listeners.listeners.forEach(async (element: ListenMessage) => {
+
+            for (let i = 0; i < listeners.length; i++) {
+                if ((member?.roles as GuildMemberRoleManager).cache.find(role => role.id === element.role)) {
+                    reaction.remove();
+                    return;
+                }
+            }
+
             try {
                 if (reaction.message.id == element.message && reaction.emoji.name == element.reaction) {
                     await member?.roles.add(element.role);
@@ -22,14 +30,14 @@ const ListenerLoader = async (client: Client) => {
             }
         })
     })
-    
+
     client.on(Events.MessageReactionRemove, async (reaction, user) => {
-        if(user.bot) return;
+        if (user.bot) return;
         if (reaction.message.guildId == null) return console.log('not found');
-    
+
         const guilds = client.guilds.cache.get(reaction.message.guildId);
         const member = guilds?.members.cache.get(user.id);
-    
+
         listeners.listeners.forEach(async (element: ListenMessage) => {
             try {
                 if (reaction.message.id == element.message && reaction.emoji.name == element.reaction) {
@@ -41,7 +49,7 @@ const ListenerLoader = async (client: Client) => {
         })
     })
 
-    console.log('Listener loaded!')
+    console.log('Listener loaded!');
 }
 
 export default ListenerLoader;
